e8ea6d4f6fb2dd921f7ef696371e2ef624d4dc06
samar-raghav <samarraghav001@gmail.com>
2025-04-14 21:29:15 +0530

    update file

---
+++ script.js
@@ -0,0 +1,22 @@
+let join =document.getElementById("join");
+join.addEventListener("click",()=>{
+
+    
+// Add content to the element
+join.innerHTML = '<h1>Hello, World!</h1>';
+
+// Append the new element to the body of the document
+document.body.append(join);
+
+
+});
+const signup=document.getElementById("Sign");
+signup.addEventListener("click",()=>{
+     window.open(`http://127.0.0.1:5502/index.html`);
+
+});
+let logo=document.getElementById("img");
+logo.addEventListener("mouseover",()=>{
+    
+
+})
